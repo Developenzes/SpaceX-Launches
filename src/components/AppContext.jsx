6ee9d2b684237ec94df73bd18fa3f5f1a2455397
Developenzes <miroslav.penzes@gmail.com>
2022-11-01 20:18:41 +0100

    save favorite to local storage

--- src/components/AppContext.jsx
+++ src/components/AppContext.jsx
@@ -1,11 +1,27 @@
 import { createContext } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const AppContext = createContext();
 
 export function AppProvider(props) {
     const [star, setStar] = useState(false)
-    const [favoriteLaunch, setFavoriteLaunch] = useState([])
+    const [favoriteLaunch, setFavoriteLaunch] = useState(() => {
+        let savedLaunch = [];
+        try {
+          savedLaunch = JSON.parse(localStorage.getItem("favoriteLaunch")) || [];
+        } catch (error) {
+          savedLaunch = [];
+        }
+        return savedLaunch;
+    });
+    
+    useEffect(() => {
+        if (favoriteLaunch) {
+          localStorage.setItem("favoriteLaunch", JSON.stringify(favoriteLaunch));
+        }
+    }, [favoriteLaunch]);
+
+
 
     const handleStarButton = (newLaunch) => {
         const existingLaunch = favoriteLaunch.find(fav => fav.id === newLaunch.id);
